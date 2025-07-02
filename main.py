844ce50224f962ebd496353b63eaf60530351395
SeixinShouei <seishinshouei@gmail.com>
2025-07-02 16:55:22 +0800

    Add files via upload
    Upload initial Flask app

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    first_choice = request.form.get('first_choice')  # Handles HIYYYAAAA / hello
+    answer = request.form.get('answer')              # Handles YES / NO
+    return render_template("index.html", choice=first_choice, answer=answer)
+
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
