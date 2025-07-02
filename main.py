42800eceb77b7b18f78e2738d5f92697917b0139
SeixinShouei <seishinshouei@gmail.com>
2025-07-02 17:05:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -8,8 +9,5 @@ def home():
     answer = request.form.get('answer')              # Handles YES / NO
     return render_template("index.html", choice=first_choice, answer=answer)
 
-import os
-
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)
-
